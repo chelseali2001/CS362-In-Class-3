560ad397ca522652c03dd3b591ca7bae0f8d9eea
Chelsea Li <chelseali2001@gmail.com>
2021-05-20 16:23:49 -0700

    Tested the unittests

--- test_leapYear.py
+++ test_leapYear.py
@@ -5,14 +5,17 @@ class SimpleTest(unittest.TestCase):
     def test_leapYear(self):
         self.assertEqual(leapYear.ly(0), "is a leap year")
 
-    def test_leapYear(self):
+    def test_leapYear0(self):
         self.assertEqual(leapYear.ly(100), "is not a leap year")
     
-    def test_leapYear(self):
+    def test_leapYear1(self):
         self.assertEqual(leapYear.ly(400), "is a leap year")
 
-    def test_leapYear(self):
+    def test_leapYear2(self):
         self.assertEqual(leapYear.ly(2004), "is a leap year")
     
-    def test_leapYear(self):
+    def test_leapYear3(self):
         self.assertEqual(leapYear.ly("2004"), "is a leap year")
+
+if __name__ == '__main__':
+    unittest.main()
